99546e192ffd5c83e0c4621d579d3bc7dd027cd6
BranceA <brancealexander@gmail.com>
2020-01-22 15:24:10 -0600

    some arrays and slices

--- playpen.go
+++ playpen.go
@@ -13,9 +13,29 @@ func main() {
 
 	var x int = 3
 
-	shortHand := 5
+	shortHand := 10
+
+	if shortHand > 10 {
+		fmt.Println("Bigger than 10")
+	} else if shortHand < 10 {
+		fmt.Println("Smaller than 10")
+	} else {
+		fmt.Println("That is 10")
+	}
 
 	fmt.Println(x)
 
 	fmt.Println(shortHand)
+
+	var testArray [5]int
+	testArray[2] = 7
+	fmt.Println(testArray)
+
+	testArrTwo := [5]int{3, 5, 8, 1, 1}
+	fmt.Println(testArrTwo)
+
+	testSlice := []int{2, 3, 4, 5, 6}
+	fmt.Println(testSlice)
+	testSlice = append(testSlice, 7)
+	fmt.Println(testSlice)
 }
