aa5e49a37a0f47af51e5618038f195f201d8b362
BranceA <brancealexander@gmail.com>
2020-01-22 15:09:43 -0600

    basic variables and shorthand

--- playpen.go
+++ playpen.go
@@ -9,5 +9,13 @@ import (
 func main() {
 	fmt.Println("My favorite number is", rand.Intn(10))
 
-	fmt.Printf("Did I get Pi? %d", math.Pi)
+	fmt.Printf("Did I get Pi? %f\n", math.Pi) 
+
+	var x int = 3
+
+	shortHand := 5
+
+	fmt.Println(x)
+
+	fmt.Println(shortHand)
 }
